b77411b1fb76008d04366dd64cc3e3f5ad10ea5a
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-06 16:55:09 +0100

    Update create_contact.php

--- create_contact.php
+++ create_contact.php
@@ -3,35 +3,35 @@
 $title = "Create contact";
 require_once("includes/header.php"); 
 
-   if (isset($_POST["crearContacte"])) {
+   if (isset($_POST["createContact"])) {
         
-      $nom = $_POST["nom"];
-      $cognoms = $_POST["cognoms"];
+      $name = $_POST["name"];
+      $lastName = $_POST["lastname"];
       $prefix = $_POST["prefix"];
-      $telefon = $_POST["telefon"];
+      $mobile = $_POST["mobile"];
       $email = $_POST["email"];
-      $categoria = $_POST["categoria"];
+      $category = $_POST["category"];
 
-      if (empty($nom) || empty($cognoms) || empty($prefix) || empty($telefon) || empty($email)) {
+      if (empty($name) || empty($lastName) || empty($prefix) || empty($mobile) || empty($email)) {
          $error = "Error. There are empty fields.";
          header('Location: create_contact.php?error=' . $error);
       }else{           
 
-         $query = "INSERT INTO contactes(nom, cognoms, prefix, telefon, email)VALUES(:nom, :cognoms, :prefix, :telefon, :email)";
+         $query = "INSERT INTO contacts(name, lastname, prefix, mobile, email)VALUES(:name, :lastname, :prefix, :mobile, :email)";
 
          $stmt = $conn->prepare($query);
 
-         $stmt->bindParam(":nom", $nom, PDO::PARAM_STR);
-         $stmt->bindParam(":cognoms", $cognoms, PDO::PARAM_STR);
+         $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+         $stmt->bindParam(":lastname", $lastname, PDO::PARAM_STR);
          $stmt->bindParam(":prefix", $prefix, PDO::PARAM_STR);
-         $stmt->bindParam(":telefon", $telefon, PDO::PARAM_STR);
+         $stmt->bindParam(":mobile", $mobile, PDO::PARAM_STR);
          $stmt->bindParam(":email", $email, PDO::PARAM_STR);
 
-         $resultat = $stmt->execute();
+         $result = $stmt->execute();
 
-         if ($resultat) {
-            $missatge = "Contact created successfully.";
-            header('Location: contacts.php?missatge=' . $missatge);
+         if ($result) {
+            $message = "Contact created successfully.";
+            header('Location: contacts.php?missatge=' . $message);
          }else{
             $error = "Error when creating the contact. There are empty fields or the email already exists.";
             header('Location: create_contact.php?error=' . $error); 
@@ -55,12 +55,12 @@ require_once("includes/header.php");
 
             <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
                 <div class="mb-3">
-                    <label for="nom" class="form-label">Name:</label>
-                    <input type="text" class="form-control" name="nom" required>
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" class="form-control" name="name" required>
                 </div>
                 <div class="mb-3">
-                    <label for="cognoms" class="form-label">Last name:</label>
-                    <input type="text" class="form-control" name="cognoms" required>
+                    <label for="lastname" class="form-label">Last name:</label>
+                    <input type="text" class="form-control" name="lastname" required>
                 </div>
                 <div class="row">
                     <div class="col-3 col-sm-2 mb-3">
@@ -68,8 +68,8 @@ require_once("includes/header.php");
                         <input type="text" class="form-control" name="prefix" placeholder="34" required>
                     </div>
                     <div class="col-9 col-sm-10  mb-3">
-                        <label for="telefon" class="form-label">Mobile:</label>
-                        <input type="text" class="form-control" name="telefon" required>
+                        <label for="mobile" class="form-label">Mobile:</label>
+                        <input type="text" class="form-control" name="mobile" required>
                     </div>
                 </div>
                 <div class="mb-3">
@@ -77,14 +77,12 @@ require_once("includes/header.php");
                     <input type="email" class="form-control" name="email" required>
                 </div>
 
-                <button type="submit" name="crearContacte" class="mt-3 btn bg-green-color w-100">Create new
+                <button type="submit" name="createContact" class="mt-3 btn bg-green-color w-100">Create new
                     contact</button>
             </form>
 
         </div>
-
     </div>
-
 </div>
 
 <?php require_once("includes/footer.php") ?>
