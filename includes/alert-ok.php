5995968b239c92fe6a1eb0e8f2ec6131da0ce50f
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-06 17:03:06 +0100

    Update alert-ok.php

--- includes/alert-ok.php
+++ includes/alert-ok.php
@@ -1,11 +1,15 @@
 <div class="row">
     <div class="col-sm-12">
-        <?php if(isset($_GET["missatge"])) : ?>
+        
+        <?php if(isset($_GET["message"])) : ?>
+            
             <div class="alert alert-ok alert-dismissible fade show p-2" role="alert">
-                <strong><?php echo $_GET["missatge"]; ?></strong> 
+                <strong><?php echo $_GET["message"]; ?></strong> 
                 <button type="button" class="btn-close" id="x-alert" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+            </div>
+        
         <?php endif; ?>
+        
     </div>  
 </div>
 
