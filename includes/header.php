4efdc40e8c88c5f91802bb736cd738c34ad30e9b
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-06 17:01:14 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="noindex">
     <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="css-contact-list/styles-contact-list.css">
     <title><?php echo $title;?></title>
