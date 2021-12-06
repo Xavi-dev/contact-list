ab2fc47a9dc9741ee1b5422ef45becbba4023a2c
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-06 17:03:40 +0100

    Update alert-error.php

--- includes/alert-error.php
+++ includes/alert-error.php
@@ -1,11 +1,15 @@
 <div class="row">
     <div class="col-sm-12">
+        
         <?php if(isset($_GET["error"])) : ?>
+        
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
                 <strong><?php echo $_GET["error"]; ?></strong> 
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+            </div>
+        
         <?php endif; ?>
+        
     </div>  
 </div>
 
