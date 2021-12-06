34b5b67898670c717a459c21324e87899f459934
Xavi <92776008+Xavi-dev@users.noreply.github.com>
2021-12-06 16:51:07 +0100

    Update delete_contact.php

--- delete_contact.php
+++ delete_contact.php
@@ -4,28 +4,28 @@ $title = "Delete contact";
 require_once("includes/header.php"); 
 
    if (isset($_GET['id'])) {
-      $idContacte = $_GET['id'];
+      $idContact = $_GET['id'];
    }
 
-   $query = "SELECT * FROM contactes WHERE id = :id";
+   $query = "SELECT * FROM contacts WHERE id = :id";
    $stmt = $conn->prepare($query);
 
-   $stmt->bindParam(":id", $idContacte, PDO::PARAM_INT);
+   $stmt->bindParam(":id", $idContact, PDO::PARAM_INT);
    $stmt->execute();
 
-   $contacte = $stmt->fetch(PDO::FETCH_OBJ);
+   $contact = $stmt->fetch(PDO::FETCH_OBJ);
 
-   if (isset($_POST["eliminarContacte"])) {        
+   if (isset($_POST["deleteContact"])) {        
        
-      $query = "DELETE FROM contactes WHERE id = :id";
+      $query = "DELETE FROM contacts WHERE id = :id";
       $stmt = $conn->prepare($query);       
-      $stmt->bindParam(":id", $idContacte, PDO::PARAM_INT);
+      $stmt->bindParam(":id", $idContact, PDO::PARAM_INT);
 
-      $resultat = $stmt->execute();
+      $result = $stmt->execute();
 
-   if ($resultat) {
-      $missatge = "Contact deleted successfully.";
-      header('Location: contacts.php?missatge=' . $missatge);
+   if ($result) {
+      $message = "Contact deleted successfully.";
+      header('Location: contacts.php?missatge=' . $message);
       exit();
    }else{
       $error = "Error, contact could not be deleted.";
@@ -50,40 +50,40 @@ require_once("includes/header.php");
             <form method="POST" action="<?php $_SERVER['PHP_SELF']; ?>">
 
                 <div class="mb-3">
-                    <label for="nom" class="form-label">Name:</label>
-                    <input type="text" class="form-control form-control-none" name="nom"
-                        value="<?php if($contacte) echo $contacte->nom; ?>" readonly>
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" class="form-control form-control-none" name="name"
+                        value="<?php if($contact) echo $contact->name; ?>" readonly>
                 </div>
                 <div class="mb-3">
-                    <label for="cognoms" class="form-label">Last name:</label>
-                    <input type="text" class="form-control form-control-none" name="cognoms"
-                        value="<?php if($contacte) echo $contacte->cognoms; ?>" readonly>
+                    <label for="lastname" class="form-label">Last name:</label>
+                    <input type="text" class="form-control form-control-none" name="lastname"
+                        value="<?php if($contact) echo $contact->lastname; ?>" readonly>
                 </div>
 
                 <div class="row">
                     <div class="col-3 col-sm-2 mb-3">
                         <label for="prefix" class="form-label">Prefix:</label>
                         <input type="text" class="form-control" name="prefix"
-                            value="<?php if($contacte) echo $contacte->prefix; ?>" readonly>
+                            value="<?php if($contact) echo $contact->prefix; ?>" readonly>
                     </div>
                     <div class="col-9 col-sm-10 mb-3">
-                        <label for="telefon" class="form-label">Mobile:</label>
-                        <input type="text" class="form-control" name="telefon"
-                            value="<?php if($contacte) echo $contacte->telefon; ?>" readonly>
+                        <label for="mobile" class="form-label">Mobile:</label>
+                        <input type="text" class="form-control" name="mobile"
+                            value="<?php if($contact) echo $contacte->mobile; ?>" readonly>
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email:</label>
                     <input type="email" class="form-control form-control-none" name="email"
-                        value="<?php if($contacte) echo $contacte->email; ?>" readonly>
+                        value="<?php if($contact) echo $contact->email; ?>" readonly>
                 </div>
 
-                <button type="submit" name="eliminarContacte" class="mt-3 btn bg-red-color w-100">Delete
+                <button type="submit" name="deleteContact" class="mt-3 btn bg-red-color w-100">Delete
                     contact</button>
+               
             </form>
         </div>
     </div>
-
 </div>
 
 <?php require_once("includes/footer.php") ?>
